db25b85fd16879ed3bc47bc3ab019202208b1b67
jw <drashadow@163.com>
2015-12-28 19:25:03 +0800

    fix localhost set cookie

--- js/background.js
+++ js/background.js
@@ -55,66 +55,68 @@ MyCookie.prototype.removeAll = function(options, cookies, callback) {
 
 
 MyCookie.prototype.timer = function(days) {
-  var timestamp = (new Date()).getTime()/1000;
+  var timestamp = (new Date()).getTime() / 1000;
   var timer = timestamp + (3600 * 24 * days);
   return timer;
 }
 
-MyCookie.prototype.checkDebug = function(currentURL,tabId, mode){
+MyCookie.prototype.checkDebug = function(currentURL, tabId, mode) {
   var self = this;
   var DEBUG = '_fdebug';
   var targetCookies = [];
-    var check_domain = currentURL.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/);
-    if(check_domain == null){
-      mylog("error domain");
+  var check_domain = currentURL.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/);
+  if (check_domain == null) {
+    mylog("error domain");
+    return;
+  }
+  var last_domain = check_domain[1];
+  var domain = (last_domain.indexOf("localhost") > -1) ? "localhost" : last_domain;
+  var set_domain = (domain == "localhost") ? "" : last_domain;
+  self.getAll({
+    'domain': domain
+  }, function(cookies) {
+    targetCookies = cookies;
+    var status = false;
+
+    cookies.some(function(cookie) {
+      if (cookie.name == DEBUG && cookie.path == '/') {
+        status = true;
+        return true;
+      }
+    });
+
+    if (mode != "clickAction") {
+      changeIcon(status, tabId);
       return;
     }
-    var domain = check_domain[1];
-    self.getAll({
-      'domain': domain
-    }, function(cookies) {
-      targetCookies = cookies;
-      var status = false;
-      
-      cookies.some(function(cookie) {
-        if (cookie.name == DEBUG && cookie.path == '/') {
-          status = true;
-          return true;
-        }
+
+
+    self.removeAll({
+      'domain': domain,
+      'name': DEBUG
+    }, targetCookies, function() {});
+    if (!status) {
+      self.set({
+        'name': DEBUG,
+        'url': currentURL,
+        'domain': set_domain,
+        'value': '1',
+        'path': '/',
+        'expirationDate': self.timer(7)
+      }, function(cookie) {
+        //document.getElementById('text').innerHTML = cookie;
       });
+    }
 
-      if (mode != "clickAction") {
-        changeIcon(status, tabId);
-        return;
-      }
-
-
-      self.removeAll({
-        'domain': domain,
-        'name': DEBUG
-      }, targetCookies, function() {});
-      if (!status) {
-        self.set({
-          'name': DEBUG,
-          'url': currentURL,
-          'domain': domain,
-          'value': '1',
-          'path': '/',
-          'expirationDate': self.timer(7)
-        }, function(cookie) {
-          //document.getElementById('text').innerHTML = cookie;
-        });
-      }
-
-      changeIcon(!status, tabId, function(){
-        chrome.tabs.getSelected(null, function(tab){
-          chrome.tabs.reload(tab.id,{},function(){
-            console.log('reload');
-          });
+    changeIcon(!status, tabId, function() {
+      chrome.tabs.getSelected(null, function(tab) {
+        chrome.tabs.reload(tab.id, {}, function() {
+          console.log('reload');
         });
       });
-
     });
+
+  });
 }
 
 
@@ -131,7 +133,9 @@ function changeIcon(status, tabId, callback) {
 }
 
 function mylog(text) {
-  chrome.browserAction.setTitle({'title': text});
+  chrome.browserAction.setTitle({
+    'title': text
+  });
 }
 
 var cookie = new MyCookie();
@@ -141,7 +145,10 @@ chrome.cookies.onChanged.addListener(function(info) {
   console.log('cookie changed');
 });
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.query({
+  active: true,
+  currentWindow: true
+}, function(tabs) {
   selectedId = tabs[0].id;
 });
 
@@ -151,13 +158,13 @@ chrome.tabs.onUpdated.addListener(function(tabId, props, tab) {
 });
 
 chrome.tabs.onSelectionChanged.addListener(function(tabId, props) {
-  chrome.tabs.getSelected(null, function(tab){
+  chrome.tabs.getSelected(null, function(tab) {
     cookie.checkDebug(tab.url, tabId, 'onSelected')
   });
 });
 
-chrome.browserAction.onClicked.addListener(function(tab){
-    var currentURL = tab.url;
-    var tabId = tab.id;
-    cookie.checkDebug(currentURL, tabId, 'clickAction');
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var currentURL = tab.url;
+  var tabId = tab.id;
+  cookie.checkDebug(currentURL, tabId, 'clickAction');
 });
